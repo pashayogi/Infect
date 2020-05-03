bf0eafefb855c0d4fd1a99fd0308cd10d123608a
Indonesia IT☑ <50790111+pashayogi@users.noreply.github.com>
2020-05-04 04:04:18 +0800

    Create update.sh

---
+++ lol/update.sh
@@ -0,0 +1,100 @@
+Skip to content
+
+pashayogi / infect
+
+#colour section
+
+red='\033[1;31m'
+
+rset='\033[0m'
+
+grn='\033[1;32m'
+
+ylo='\033[1;33m'
+
+#script coding starts
+
+clear
+
+echo " "
+
+echo " "
+
+echo '
+
+                  _  _  ___  ___   __  ____  ___ 
+
+                 ( )( )(  ,\(   \ (  )(_  _)(  _)
+
+                  )()(  ) _/ ) ) )/__\  )(   ) _)
+
+                  \__/ (_)  (___/(_)(_)(__) (___) v 1.0
+
+'|lolcat
+
+echo " "
+
+echo " "
+
+sleep 6.0
+
+clear
+
+echo -e "$red                         ChEcKiNg..>$rset"
+
+sleep 2.0
+
+clear
+
+echo -e "$red                         ChEcKiNg...>$rset"
+
+sleep 2.0
+
+clear
+
+echo -e "$red                         ChEcKiNg....>$rset"
+
+sleep 2.0
+
+clear
+
+echo -e "$red                         ChEcKiNg.....>$rset"
+
+sleep 2.0
+
+clear
+
+echo " "
+
+echo " "
+
+echo -e "$grn                memperbarui infect tools, harap tunggu$rset"
+
+sleep 2.0
+
+cd $HOME
+
+rm -rf Infect
+
+git clone https://github.com/pashayogi/Infect
+
+clear
+
+echo " "
+
+echo -e "$grn               infect telah diperbarui periksa sekarang ...$rset"
+
+sleep 3.0
+
+echo " "
+
+cd $HOME
+
+cd Infect
+
+bash infect.sh
+
+clear
+
+sleep 1.0
+
